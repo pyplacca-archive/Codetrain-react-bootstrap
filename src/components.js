66bae874cdfb50894eb814d4bb6b25ece528437e
pyPlacca <dhegreatprosperity10@gmail.com>
2020-06-10 22:15:07 +0000

    initial commit

---
+++ src/components.js
@@ -0,0 +1,61 @@
+import React from 'react'
+
+
+function Category({name}) {
+	const icn = name.split(' ').join('-').toLowerCase()
+	const bg = `url("./assets/icons/${
+		icn.endsWith('s') ? icn.substring(0, icn.length-1) : icn
+	}.png")`
+
+	return (
+		<p 
+			className="category" 
+			style={{backgroundImage: bg}}
+		>
+			{name}
+		</p>
+	)
+}
+
+
+const numFormat = Intl.NumberFormat().format
+
+class Product extends React.Component {
+	render () {
+		const { discount, image, name, quantity, pre_price } = this.props
+		const price = pre_price - ((discount/100) * pre_price).toFixed()
+
+		return (
+			<div className="product">
+				{
+					discount ? <p className="discount-percent">{discount}%</p> : ''
+				}
+				
+				<img className="item-image" src={image} alt={name} />
+				
+				<h5 className="item-name">{
+					name.length > 32 ? name.substr(0, 29) + '...' : name 
+				}</h5>
+				<p className="item-quantity">{quantity} pc{quantity > 1 ? 's' : ''}</p>
+				
+				<div className="item-checkout">
+					<p 
+						className="amount" 
+						price-before={discount ? '$' + numFormat(pre_price) : ''}
+					>
+						${numFormat(price || pre_price)}
+					</p>
+				
+					<p className="add-to-cart" 
+						style={{backgroundImage: "url('./assets/icons/shopping-bag.png')"}}
+					>
+						Cart
+					</p>
+				</div>
+			</div>
+		)
+	}
+}
+
+
+export { Category, Product }
---
+++ src/data-source.js
@@ -0,0 +1,88 @@
+import React from 'react' 
+
+
+const product_categories = [
+	'purse', 
+	'hand bags', 
+	'shoulder bags', 
+	'wallet', 
+	'laptop bags'
+]
+
+const listings = [
+	{
+		name: 'Advent Laptop Bag',
+		quantity: 5,
+		pre_price: 500,
+		discount: 20,
+		image: './assets/images/laptop-bags/advent.jpg',
+	},
+	{
+		name: 'Leatherology Bi-fold Wallet',
+		quantity: 11,
+		pre_price: 280,
+		discount: 5,
+		image: './assets/images/wallets/vertical-bifold.jpg',
+	},
+	{
+		name: 'Median Large - Tan Handbag',
+		quantity: 1,
+		pre_price: 995,
+		image: './assets/images/handbags/median-tan.jpg',
+	},
+	{
+		name: 'Victorine Wallet',
+		quantity: 3,
+		pre_price: 780,
+		discount: 13,
+		image: './assets/images/wallets/victorine.jpg',
+	},
+	{
+		name: 'McJolts Matte Shoulder Bag',
+		quantity: 1,
+		pre_price: 370,
+		discount: 10,
+		image: './assets/images/shoulder-bags/matteyy.jpg',
+	},
+	{
+		name: 'Dolce F Laptop Bag',
+		quantity: 2,
+		pre_price: 419,
+		discount: 50,
+		image: './assets/images/laptop-bags/chic-grey.jpg',
+	},
+	{
+		name: 'Dune London Handbag',
+		quantity: 1,
+		pre_price: 1200,
+		// discount: ,
+		image: './assets/images/handbags/dune-london.jpg',
+	},
+	{
+		name: 'Groche Snoflake Wallet',
+		quantity: 2,
+		pre_price: 650,
+		discount: 8,
+		image: './assets/images/wallets/snowflake.jpg',
+	},
+	{
+		name: 'Chevron Blue Laptop Bag',
+		quantity: 7,
+		pre_price: 300,
+		discount: 5,
+		image: './assets/images/laptop-bags/chevron-blue.jpg',
+	},
+	{
+		name: 'St. Jacobs Shoulder Bag',
+		quantity: 13,
+		pre_price: 360,
+		discount: 3,
+		image: './assets/images/shoulder-bags/campy.jpg',
+	},
+]
+
+
+export {
+	product_categories,
+	listings
+}	

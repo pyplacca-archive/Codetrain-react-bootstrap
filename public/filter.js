0468599a2b1d4fde31cebfc4c0bd720855382bea
pyPlacca <dhegreatprosperity10@gmail.com>
2020-06-12 01:28:00 +0000

    refactors code

--- public/filter.js
+++ public/filter.js
@@ -1,47 +1,40 @@
-const temp_storage = []
-
-document.getElementById('sidebar').addEventListener(
-	'click', 
-	({target}) => {
-		if (target.classList.contains('category')) {
-			applyFilter(target)
-		}
-	}
-)
-
-
-const container = document.getElementById('product-list')
-const counter = new DOMParser().parseFromString(
-	'<span class="counter"></span>',
-	'text/html'
-).body.firstElementChild
+const [
+	temp_storage,
+	container, 
+	counter
+] = [
+	[],
+	document.getElementById('product-list'),
+	document.createElement('span')
+]
+counter.setAttribute('class', 'counter')
 
 
 function applyFilter(elm) {
 	const cls = 'filter-active'
-	// first remove class name from previous element...
+	// first remove class name (cls) from previous element...
 	const previous = document.querySelector('.' + cls)
-	if (previous) {
-		previous.classList.remove(cls)
-	}
+	previous ? previous.classList.remove(cls) : null
 	// then append it to the selected category...
 	elm.classList.add(cls)
 	// then filter listed items
-	filterItems(elm.getAttribute('filter_name'), elm)
+	filterItems(
+		elm.getAttribute('filter_name'), 
+		elm
+	)
 }
 
 function filterItems(category, sender) {
 	if (category !== 'all products') {
 		// retrieve any item that belongs to the selected category from temp_storage
 		temp_storage.reduce((arr, elt, i) => {
-			if (elt.getAttribute('tags').includes(category)) {
-				container.appendChild(elt)
-			} else {
-				arr.push(elt)
-			}
+			elt.getAttribute('tags').includes(category) ?
+				container.appendChild(elt) :
+					arr.push(elt)
 			return arr
 		}, []);
-		// then removes the ones that don't belong to the selected category from the container (product-list)
+		// then remove the items that don't belong to the 
+		// selected category from the container (product-list)
 		[...container.children].forEach(item => {
 			const tags = item.getAttribute('tags')
 
@@ -58,10 +51,19 @@ function filterItems(category, sender) {
 	setCounter(sender)
 }
 
-
 function setCounter (sender) {
 	counter.innerText = container.children.length
 	sender.appendChild(counter)
 }
 
+
 setCounter(document.querySelector('.category'))
+
+document.getElementById('sidebar').addEventListener(
+	'click', 
+	({target}) => {
+		target.classList.contains('category') ?
+		applyFilter(target) : 
+		null
+	}
+)
